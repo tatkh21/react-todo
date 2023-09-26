f97ac4b9c4ab925b8268b9c52ba858c49b2c6024
tatkh21 <tatkh21@gmail.com>
2023-09-25 22:53:59 -0500

    Adding list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-
+import React from "react";
+const todoList = [
+  {
+    id: 1,
+    title: " Cook dinner",
+  },
+  {},
+  {},
+];
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      ...
+      <hr />
+      <ul>
+        {todoList.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
